57c0dd894849205930dade0cd687d04d8cecf4ee
n-ryota1375 <136418952+n-ryota1375@users.noreply.github.com>
2023-07-11 10:43:03 +0900

    completed

--- client/index.js
+++ client/index.js
@@ -6,9 +6,16 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const merkleTree = new MerkleTree(niceList);
+  const name = 'Owen Parker DDS';
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
+  //console.log(proof);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name: name,
+    proof: proof,
   });
 
   console.log({ gift });
